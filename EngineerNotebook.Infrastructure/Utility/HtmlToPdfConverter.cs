00f7d670fcb77e67918bd0630a94868e739965a7
JBraunsmaJr <jonbraunsma@gmail.com>
2022-02-11 12:27:50 -0500

    Updated pdf strategy

--- EngineerNotebook.Infrastructure/Utility/HtmlToPdfConverter.cs
+++ EngineerNotebook.Infrastructure/Utility/HtmlToPdfConverter.cs
@@ -1,26 +1,35 @@
-using System.IO;
-using System.Threading.Tasks;
 using EngineerNotebook.Core.Interfaces;
-using SelectPdf;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace EngineerNotebook.Infrastructure.Utility
 {
     public class HtmlToPdfConverter : IHtmlToPdfConverter
     {
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-        public async Task<byte[]> HtmlToPdf(string html)
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        private readonly HttpClient _client;
+        private readonly string _converterApi;
+        private ILogger<HtmlToPdfConverter> _logger;
+        public HtmlToPdfConverter(IConfiguration config, ILogger<HtmlToPdfConverter> logger)
         {
-            HtmlToPdf converter = new HtmlToPdf();
-            converter.Options.MarginBottom = 10;
-            converter.Options.MarginLeft = 10;
-            converter.Options.MarginRight = 10;
+            _logger = logger;
+            _converterApi = config.GetValue<string>("ConverterAPI");
+            _client = new();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
+        }
+        
+        public async Task<byte[]> HtmlToPdf(string html)
+        {
+            var response = await _client.PostAsync(_converterApi, new StringContent(html, Encoding.UTF8, "text/html"));
 
-            PdfDocument doc = converter.ConvertHtmlString(html);
-
-            using MemoryStream stream = new MemoryStream();
-            doc.Save(stream);
-            return stream.ToArray();
+            if (response.IsSuccessStatusCode)
+                return await response.Content.ReadAsByteArrayAsync();
+            
+            _logger.LogWarning($"Failed to convert HTML: {response.StatusCode} | {response.ReasonPhrase}");
+            
+            return null;
         }
     }
 }
