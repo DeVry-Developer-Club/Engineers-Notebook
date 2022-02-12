e5493f119b0c6c07fb1e1c656908ed56c2d94f6b
JBraunsmaJr <jonbraunsma@gmail.com>
2022-02-11 20:34:00 -0500

    Committing working approach, room for improvement

--- EngineerNotebook.Infrastructure/Utility/HtmlToPdfConverter.cs
+++ EngineerNotebook.Infrastructure/Utility/HtmlToPdfConverter.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using EngineerNotebook.Core.Interfaces;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -16,20 +17,50 @@ namespace EngineerNotebook.Infrastructure.Utility
         {
             _logger = logger;
             _converterApi = config.GetValue<string>("ConverterAPI");
-            _client = new();
+
+            HttpClientHandler handler = new();
+            handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+            handler.ServerCertificateCustomValidationCallback = (_, _, _, _) => true;
+            
+            _client = new(handler);
             _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html"));
         }
         
         public async Task<byte[]> HtmlToPdf(string html)
         {
-            var response = await _client.PostAsync(_converterApi, new StringContent(html, Encoding.UTF8, "text/html"));
+            var request = WebRequest.Create(_converterApi) as HttpWebRequest;
+            request.ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true;
+            
+            // initialize content
+            var contentBuffer = Encoding.UTF8.GetBytes(html);
+            request.ContentType = "text/html";
+            request.ContentLength = contentBuffer.Length;
+            request.Method = "POST";
 
-            if (response.IsSuccessStatusCode)
-                return await response.Content.ReadAsByteArrayAsync();
+            await using (var requestStream = await request.GetRequestStreamAsync())
+            {
+                await requestStream.WriteAsync(contentBuffer, 0, contentBuffer.Length);
+                await requestStream.FlushAsync();
+                requestStream.Close();
+            }
             
-            _logger.LogWarning($"Failed to convert HTML: {response.StatusCode} | {response.ReasonPhrase}");
-            
-            return null;
+            // Get the response / content
+            using (var httpResponse = await request.GetResponseAsync() as HttpWebResponse)
+            {
+                // check for error status
+                if (httpResponse.StatusCode != HttpStatusCode.OK)
+                {
+                    _logger.LogCritical($"Error converting to pdf... {httpResponse.StatusCode} | {httpResponse.StatusDescription}");
+                    return null;
+                }
+
+                await using (var response = httpResponse.GetResponseStream())
+                {
+                    await using MemoryStream stream = new();
+                    await response.CopyToAsync(stream);
+                    return stream.ToArray();
+                }
+            }
         }
     }
 }
